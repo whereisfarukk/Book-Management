0eaf313a294ce24caa9406bf04c205d8c0d3f082
whereisfarukk <whereisfarukk@gmail.com>
2025-07-18 23:48:16 +0600

    �fix(auth): preserve intended path using location state

--- Client/src/routers/guards/PrivateRoutes.jsx
+++ Client/src/routers/guards/PrivateRoutes.jsx
@@ -6,11 +6,12 @@ import { Loader } from "../../components/Ui/Loader";
 export const PrivateRoutes = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
     const location = useLocation();
+    console.log(location);
     if (loading) {
         return <Loader />;
     }
     if (user) {
         return children;
     }
-    return <Navigate to="/login"></Navigate>;
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>; // location refers to the current location the user was trying to visit (like /admin/dashboard)
 };
